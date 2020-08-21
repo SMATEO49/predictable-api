f76f775f3e97e2384dc61a7db77dc4fe896b8ed6
smate <smateusz49@interia.eu>
2020-08-21 12:43:31 +0200

    bitcoin.csv 1-5-2013 to 15-8-2020

--- utils/csv_maker.py
+++ utils/csv_maker.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 from coin_gecko import *
+import time
 
 
 colnames = ['number', 'date']
@@ -11,6 +12,7 @@ i = 1216
 a = [0, 0, 0, 0, 0, 0, 0, 0]
 n = 0           # acumulated values in a
 w = 0           # wroten useful rows(0-7)
+j = 1
 
 with open('../data/bitcoin.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
@@ -18,6 +20,7 @@ with open('../data/bitcoin.csv', 'w', newline='') as csvfile:
     while i < lenght-7:
         try:
             b = get_single_price('bitcoin', df.loc[i, "date"])
+            time.sleep(1)
             try:
                 a[n] = b
                 n = n + 1
@@ -25,29 +28,47 @@ with open('../data/bitcoin.csv', 'w', newline='') as csvfile:
                 if w == 0:
                     writer.writerow([a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 1:
                     writer.writerow([a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[0]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 2:
                     writer.writerow([a[2], a[3], a[4], a[5], a[6], a[7], a[0], a[1]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 3:
                     writer.writerow([a[3], a[4], a[5], a[6], a[7], a[0], a[1], a[2]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 4:
                     writer.writerow([a[4], a[5], a[6], a[7], a[0], a[1], a[2], a[3]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 5:
                     writer.writerow([a[5], a[6], a[7], a[0], a[1], a[2], a[3], a[4]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 6:
                     writer.writerow([a[6], a[7], a[0], a[1], a[2], a[3], a[4], a[5]])
                     a[w] = b
+                    print(j)
+                    j += 1
                 elif w == 7:
                     writer.writerow([a[7], a[0], a[1], a[2], a[3], a[4], a[5], a[6]])
                     a[w] = b
                     w = -1
-
+                    print(j)
+                    if j%42==0:
+                        print('Sleeping :) ZzZ')
+                        time.sleep(59)
+                    j += 1
                 w = w+1
 
         except KeyError:
